73689cfa43399076a36f5918bd02147c7e698c2b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:23:03 +0300

    website

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    pass
 
+def home_page():
+	players = ["me", "myself", "i"]
+	return render_template("index.html",
+		players = players)
+	
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
